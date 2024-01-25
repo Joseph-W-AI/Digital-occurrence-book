7783f0e6b6273eb168dcc23538da75b502def348
Joseph-W-AI <145222364+Joseph-W-AI@users.noreply.github.com>
2024-01-25 22:08:24 +0300

    Add files via upload
    initialize project

---
+++ Digital-Occurance-Book/MyApp/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, marshal_with, fields
+from flask import request, current_app as app
+import jwt
+from functools import wraps
+import datetime
+from .models import User, Incident, app,api,db
+from .controllers import Register,Login,IndividualUser,Incidents,AllUsers,IndividualIncident
+
+
+api.add_resource(Register, '/register')
+api.add_resource(Login, '/login')
+
+
+api.add_resource(AllUsers, '/users')
+api.add_resource(IndividualUser, '/users/<int:user_id>')
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
